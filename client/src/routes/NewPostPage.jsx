6fed846459641f40adafaa19b51e5b061739b794
Harshit Bakrania <harshit.bakrania@gmail.com>
2024-07-20 22:50:33 +0530

    upload image component added

--- client/src/routes/NewPostPage.jsx
+++ client/src/routes/NewPostPage.jsx
@@ -4,8 +4,11 @@ import NavBar from "../components/NavBar";
 import { SelectFilter } from "../components/SelectFilter";
 import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
+import UploadWidget from "../components/UploadImage";
+import { useState } from "react";
 
 export const NewPostPage = () => {
+    const[images, setImages] = useState([]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -13,12 +16,16 @@ export const NewPostPage = () => {
         const inputs = Object.fromEntries(formData);
         console.log(inputs)
     }
+
+    const handleDeleteImage = (url) => {
+        setImages((prev) => prev.filter((image) => image !== url));
+    }
  
     return (
        <div className="bg-background-color">
            <NavBar />
            <div className="grid grid-cols-3">
-            <div className="col-span-2">
+            <div className="col-span-2 border-r border-slate-600">
                 <div className="text-3xl font-semibold text-white px-36 pt-20">
                     Create a new Listing
                 </div>
@@ -44,22 +51,49 @@ export const NewPostPage = () => {
                     <InputBox label="Schools Nearby" type="number" name="school" />
                     <InputBox label="Hospitals Nearby" type="number" name="hospital" />
                     <InputBox label="Restaurants Nearby" type="number" name="restaurant" /> 
-                </form>
-                <div className="px-32 py-14 text-white space-y-2">
-                    <div className="text-2xl">
-                        Write a description
+                    <div className="col-span-3">
+                        <div className="py-10 text-white space-y-2 ">
+                            <div className="text-2xl">
+                                Write a description
+                            </div>
+                            <ReactQuill theme="snow" name="description" />
+                        </div>
+                        <div className="max-w-40 mx-auto pb-20">
+                            <Button label="Submit" />
+                        </div>
                     </div>
-                    <ReactQuill theme="snow" name="description" />
-                </div>
-                <div className="max-w-40 mx-auto pb-20">
-                    <Button label="Submit" />
-                </div>
-               
+                </form>               
             </div>
-            <div className="col-span-1 "> 
-                TEST
+            <div className="col-span-1 space-y-8 flex flex-col py-14 "> 
+                {images.map((image, index) => (
+                    <div key={index} className="relative hover:cursor-pointer flex space-x-3 px-24">
+                        <img src={image} className="w-80 h-45 rounded-lg" alt={`Uploaded ${index}`} />
+                        <div onClick={() => handleDeleteImage(image)}>
+                            <XMark />
+                        </div>
+                    </div>    
+                ))}
+                <div className="flex justify-center"> 
+                    <UploadWidget
+                        uwConfig={{
+                        cloudName: "dwos6fgt6",
+                        uploadPreset: "estate",
+                        multiple: true,
+                        folder: "posts",
+                        }}
+                        setState={setImages}
+                    />      
+                </div>
+                         
             </div>
            </div>   
        </div>
     );
+}
+
+function XMark(){
+    return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+  </svg>
+  
 }
