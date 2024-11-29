a0f957aa8461b7a3fc0a78bc31a781816dd88771
Harshit Bakrania <harshit.bakrania@gmail.com>
2024-11-29 21:55:41 +0530

    fixed map pin bug

--- client/src/components/Pin.jsx
+++ client/src/components/Pin.jsx
@@ -1,9 +1,22 @@
 import { Marker, Popup } from "react-leaflet";
 import { Link } from "react-router-dom";
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+import { Icon } from "leaflet";
 
 export default function Pin({item}){
+
+    const defaultIcon = new Icon({
+        iconUrl: markerIcon,
+        shadowUrl: markerShadow,
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    })
+
     return(
-        <Marker position={[item.latitude, item.longitude]}>
+        <Marker position={[item.latitude, item.longitude]} icon={defaultIcon}>
             <Popup>
                 <div>
                     <Link to={`/posts/${item.id}`}>{item.title}</Link>
@@ -13,5 +26,4 @@ export default function Pin({item}){
             </Popup>
         </Marker>
     )
-
 }
