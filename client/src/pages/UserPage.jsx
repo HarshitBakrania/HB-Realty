34851cd5730c1db7c9777ee25f8a4ed4912694c1
Harshit Bakrania <harshit.bakrania@gmail.com>
2025-02-18 23:45:50 +0530

    user page responsive

--- client/src/pages/UserPage.jsx
+++ client/src/pages/UserPage.jsx
@@ -1,89 +1,87 @@
 import { useContext } from "react";
-import Button from "../components/Button"
+import Button from "../components/Button";
 import { AuthContext } from "../context/AuthContext";
-import { Navigate, useNavigate } from "react-router-dom"
-import axios from "axios"
+import { Navigate, useNavigate } from "react-router-dom";
+import axios from "axios";
 import NavBar from "../components/NavBar";
 import { useUserPosts } from "../hooks/useUserPosts";
 import Footer from "../components/Footer";
 import PropertyCard from "../components/PropertyCard";
 
-export const UserPage = () =>{
-    const { posts, loading} = useUserPosts();
-    const userPosts = posts.userPosts;
-    const savedPosts = posts.savedPosts;
-    const {updateUser, currentUser, logout} = useContext(AuthContext);
-    const navigate = useNavigate();
+export const UserPage = () => {
+  const { posts, loading } = useUserPosts();
+  const userPosts = posts.userPosts;
+  const savedPosts = posts.savedPosts;
+  const { updateUser, currentUser, logout } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-    if(!currentUser){
-        return <Navigate to="/signin"/>
+  if (!currentUser) {
+    return <Navigate to="/signin" />;
+  }
+
+  async function LogoutUser() {
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_BACKEND_URL}/api/auth/logout`,
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+      logout();
+      navigate("/");
+    } catch (error) {
+      console.log(error);
     }
-    async function LogoutUser(){
-        try{
-            const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/auth/logout`,{
-            },{
-                withCredentials: true
-            });
-            logout();
-            navigate("/")
-        }catch(error){
-            console.log(error)
-        }  
-    }
-    
-    return(
-        <div>
-            <NavBar/>
-            <div className="text-white grid grid-cols-4 bg-background-color">
-                <div className="col-span-3">
-                    <div className="py-16 px-24 flex justify-between">
-                        <div className="text-4xl font-semibold">
-                            Your Listings
-                        </div>
-                        <Button label="Create New Listing" className="text-lg" onClick={() =>{
-                            navigate("/posts/create")
-                        }}/>
-                    </div>
-                    <div className="mx-24 mt-8 mb-28 space-y-12">
-                        {userPosts ? userPosts.map(item => (
-                            <PropertyCard key={item.id} item={item} />
-                        )) : null}
-                    </div>
-                    <div className="text-4xl font-semibold mx-24 pb-6 pt-12 border-t border-slate-800">
-                        Saved Posts
-                    </div>
-                    <div className="mx-24 mt-8 mb-36 space-y-12">
-                        {savedPosts ? savedPosts.map(item => (
-                            <PropertyCard key={item.id} item={item} />
-                        )): null}
-                    </div>
-                </div> 
-                <div className="col-span-1 bg-secondary-color max-h-max p-10 space-y-5 m-10">
-                    <div className="flex justify-between">
-                        <div className="text-3xl font-bold">
-                            Account Information
-                        </div>
-                        <Button label="Update Profile" onClick={() => navigate("/user/update")} />
-                    </div>
-                    <div className="text-xl font-semibold">
-                        <div className="flex">
-                            <div className="py-6">Avatar: </div>
-                            {currentUser.avatar ? <img src ={currentUser.avatar} className="rounded-full size-20 ml-4" /> : null}
-                        </div>
-                        <div className="pt-4">
-                            Username: {currentUser.username}
-                        </div>
-                        <div className="py-6">
-                            Email Address: {currentUser.email}
-                        </div>
-                        <div className="max-w-24 pt-2 font-normal">
-                            <Button label="Logout" onClick={LogoutUser} />
-                        </div>      
-                    </div>
-                </div>         
+  }
+
+  return (
+    <div>
+      <NavBar />
+      <div className="text-white bg-background-color lg:grid lg:grid-cols-4 lg:px-14">
+        <div className="bg-secondary-color max-h-max p-10 space-y-5 lg:col-span-1 lg:m-10 lg:order-last">
+          <div className="flex justify-between">
+            <div className="text-2xl lg:text-3xl font-bold">Account Information</div>
+            <Button label="Update Profile" onClick={() => navigate("/user/update")} />
+          </div>
+          <div className="text-lg lg:text-xl font-semibold">
+            <div className="flex">
+              <div className="py-6">Avatar:</div>
+              {currentUser.avatar ? (
+                <img src={currentUser.avatar} className="rounded-full size-20 ml-4" />
+              ) : null}
             </div>
-            <Footer/>    
+            <div className="pt-4">Username: {currentUser.username}</div>
+            <div className="py-6">Email Address: {currentUser.email}</div>
+            <div className="pt-2 font-normal">
+              <Button label="Logout" onClick={LogoutUser} />
+            </div>
+          </div>
         </div>
-    )
-}
 
+        <div className="lg:col-span-3">
+          <div className="py-8 px-6 lg:py-16 lg:px-10 flex justify-between">
+            <div className="text-2xl lg:text-4xl font-semibold">Your Listings</div>
+            <Button
+              label="Create New Listing"
+              className="lg:text-lg"
+              onClick={() => {
+                navigate("/posts/create");
+              }}
+            />
+          </div>
+          <div className="px-6 lg:px-8 mt-6 lg:mt-8 mb-20 space-y-12">
+            {userPosts ? userPosts.map((item) => <PropertyCard key={item.id} item={item} />) : null}
+          </div>
+          <div className="text-2xl lg:text-4xl font-semibold text-center pb-6 pt-12 border-t border-slate-800 mx-4">
+            Saved Posts
+          </div>
+          <div className="px-6 lg:px-8 mt-6 lg:mt-8 pb-36 space-y-12">
+            {savedPosts ? savedPosts.map((item) => <PropertyCard key={item.id} item={item} />) : null}
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
