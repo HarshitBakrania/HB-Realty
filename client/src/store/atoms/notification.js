c7c0f111d2caa1d06404d8bf9123546b03b3f859
Harshit Bakrania <harshit.bakrania@gmail.com>
2026-09-20 23:47:20 +0530

    fixed notification bug

--- client/src/store/atoms/notification.js
+++ client/src/store/atoms/notification.js
@@ -1,23 +1,37 @@
 import axios from "axios";
-import { useContext } from "react";
 import { atom, selector } from "recoil";
-import { AuthContext } from "../../context/AuthContext";
 
 export const notificationAtom = atom({
   key: "notificationAtom",
   default: selector({
     key: "notificationAtomSelector",
     get: async () => {
-      const { currentUser } = useContext(AuthContext);
-      if (currentUser) {
-        const res = await axios.get(
-          `${import.meta.env.VITE_BACKEND_URL}/api/users/notification`,
-          {
-            withCredentials: true,
-          }
-        );
-        return res.data;
+      const storedData = JSON.parse(localStorage.getItem("user") || "null");
+      const sevenDays = 1000 * 60 * 60 * 24 * 7;
+      let currentUser = null;
+
+      if (storedData && storedData.timestamp) {
+        const timeDifference = new Date().getTime() - storedData.timestamp;
+        if (timeDifference < sevenDays) {
+          currentUser = storedData.user;
+        }
       }
+
+      if (currentUser) {
+        try {
+          const res = await axios.get(
+            `${import.meta.env.VITE_BACKEND_URL}/api/users/notification`,
+            {
+              withCredentials: true,
+            }
+          );
+          return res.data;
+        } catch (error) {
+          console.log("Failed to fetch notifications:", error);
+          return 0;
+        }
+      }
+      return 0;
     },
   }),
 });
